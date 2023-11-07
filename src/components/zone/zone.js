3d104943fbaee13a077c3858d6d9b2920f6f5543
Iurii Burlacov <yura.kkrut.2003@gmail.com>
2023-11-07 17:28:04 +0200

    All cells are sorted

--- src/components/zone/zone.js
+++ src/components/zone/zone.js
@@ -7,7 +7,11 @@ function Zone(props) {
         const itemsInThisCell = props.cells.filter(item => item.joinedWith === aria.id);
         return { ...aria, items: itemsInThisCell };
     });
-    const cells=sortedCells.map(cell =>(<CellParent sku={cell.sku} defaultSku={cell.defaultSku} joinedWith={cell.joinedWith} childrens={cell.items} status={cell.status}/>))
+    const sortedWithEmptyItemsAtEnd=[
+        ...sortedCells.filter(cell => cell.items.length > 0),
+        ...sortedCells.filter(cell => cell.items.length === 0)
+    ]
+    const cells=sortedWithEmptyItemsAtEnd.map(cell =>(<CellParent sku={cell.sku} defaultSku={cell.defaultSku} joinedWith={cell.joinedWith} childrens={cell.items} status={cell.status}/>))
     return (
         <div className="zone">
             <h1>{props.name}</h1>
